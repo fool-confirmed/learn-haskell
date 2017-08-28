0dd567d5799fcc758d0b668e720ade4a0ea9a51d
fool-confirmed <>
2017-08-28 14:19:22 -0400

    w2 done

--- w2/LogAnalysis.hs
+++ w2/LogAnalysis.hs
@@ -32,8 +32,8 @@ parseMessage m
 
 -- Ex 2
 insert :: LogMessage -> MessageTree -> MessageTree
-insert lm Leaf = Node Leaf lm Leaf
 insert (Unknown _) mt = mt
+insert lm Leaf = Node Leaf lm Leaf
 insert lm (Node lt m rt) 
   | (lmGetTime lm) <= (lmGetTime m) = Node lt lm (Node Leaf m rt)
   | (lmGetTime lm) > (lmGetTime m) = Node lt m (insert lm rt)
@@ -47,13 +47,40 @@ lmGetTime :: LogMessage -> TimeStamp
 lmGetTime (LogMessage _ ts _) = ts
 lmGetTime (Unknown _) = 0
 
-foo1, foo2 :: LogMessage
-foo1 = parseMessage "E 100 123 error foo"
-foo2 = parseMessage "I 234 information foo"
-foo3 = parseMessage "this is an unknow message"
+-- Ex 3
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (x:xs) = insert x (build xs)
+
+-- Ex 4
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node _ m rt) = m : inOrder(rt)
+
+-- Ex 5
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong [] = []
+whatWentWrong ((Unknown _):xs) = whatWentWrong xs
+whatWentWrong ((LogMessage (Error i) _ m):xs) 
+  | i >= 50 = m : whatWentWrong xs
+  | otherwise = whatWentWrong xs
+whatWentWrong ((LogMessage _ _ _):xs) = whatWentWrong xs
+
+-- test code
+foo1, foo2, foo3, foo4, foo5, foo6, foo7, foo8, foo9, foo10, foo11 :: LogMessage
+foo1 = parseMessage "I 6 Completed armadillo processing"
+foo2 = parseMessage "I 1 Nothing to report"
+foo3 = parseMessage "E 99 10 Flange failed!"
+foo4 = parseMessage "I 4 Everything normal"
+foo5 = parseMessage "I 11 Initiating self-destruct sequence"
+foo6 = parseMessage "E 70 3 Way too many pickles"
+foo7 = parseMessage "E 65 8 Bad pickle-flange interaction detected"
+foo8 = parseMessage "W 5 Flange is due for a check-up"
+foo9 = parseMessage "I 7 Out for lunch, back in two time steps"
+foo10 = parseMessage "E 20 2 Too many pickles"
+foo11 = parseMessage "I 9 Back from lunch"
+
 
 myMT :: MessageTree
-myMT = Node (Node Leaf foo1 Leaf) foo2 Leaf
-
-
--- parse :: String -> [LogMessage]
+myMT = build [foo1, foo2, foo3, foo4, foo5, foo6, foo7, foo8, foo9, foo10, foo11]
+-- ghci: whatWentWrong (inOrder myMT)
